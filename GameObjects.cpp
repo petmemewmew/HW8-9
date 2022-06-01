3a4b0ef32804e463fb1badec6c3db0b3fd8e5345
petmemewmew <90765384+petmemewmew@users.noreply.github.com>
2022-06-01 10:51:13 +0800

    Update GameObjects.cpp

--- GameObjects.cpp
+++ GameObjects.cpp
@@ -1,10 +1,16 @@
 #include "GameObjects.h"
 
-GameObject::GameObject(int imageID, int x, int y, int direction, int layer, double size): ObjectBase(imageID,x,y,direction,layer,size){};
+GameObject::GameObject(int imageID, int x, int y, int direction, int layer, double size, GameWorld* world): ObjectBase(imageID,x,y,direction,layer,size){
+    this_world = world;
+};
 
-Dawnbreaker::Dawnbreaker(): GameObject(IMGID_DAWNBREAKER, 300,100, 0, 0, 1.0) {
+Dawnbreaker::Dawnbreaker(GameWorld* world): GameObject(IMGID_DAWNBREAKER, 300,100, 0, 0, 1.0,world) {
     HP = 100;
     energy = 10;
+}
+
+void GameObject::clear() {
+    delete this;
 }
 
 int GameObject::check_dead() {
@@ -16,6 +22,31 @@ void Dawnbreaker::Update() {
         is_dead = 1;
         return;
     }else{
+        if(this_world->GetKey(KeyCode::UP)) {
+            if (GetY() + 4 <= (WINDOW_HEIGHT - 1)) {
+                MoveTo(GetX(), GetY() + 4);
+            }
+        }
+        if(this_world->GetKey(KeyCode::DOWN)) {
+            if (GetY() - 4 >= 50) {
+                MoveTo(GetX(), GetY() - 4);
+            }
+        }
+        if(this_world->GetKey(KeyCode::LEFT)) {
+            if (GetX() - 4 >= 0) {
+                MoveTo(GetX() - 4, GetY());
+            }
+        }
+        if(this_world->GetKey(KeyCode::RIGHT)) {
+            if (GetX() + 4 <= (WINDOW_WIDTH - 1)) {
+                MoveTo(GetX() + 4, GetY());
+            }
+        }
+        if(this_world->GetKey(KeyCode::FIRE1)){
+            if(energy >= 10){
+                this_world->add_item(new Blue_Bullet(GetX(), GetY()+50, 0.5, 5, this_world));
+            }
+        }
         if(energy < 10){
             energy++;
         }
@@ -23,7 +54,7 @@ void Dawnbreaker::Update() {
     }
 }
 
-Star::Star(int x, int y, double size) : GameObject(IMGID_STAR, x , y, 0, 4, size){};
+Star::Star(int x, int y, double size, GameWorld* world) : GameObject(IMGID_STAR, x , y, 0, 4, size, world){};
 
 void Star::Update() {
     if(GetY() < 0){
@@ -34,7 +65,7 @@ void Star::Update() {
     }
 }
 
-Blue_Bullet::Blue_Bullet(int x, int y, double size, int hit) : GameObject(IMGID_BLUE_BULLET ,x , y, 0, 1, size){
+Blue_Bullet::Blue_Bullet(int x, int y, double size, int hit, GameWorld* world) : GameObject(IMGID_BLUE_BULLET ,x , y, 0, 1, size, world){
     hurt = hit;
 }
 
@@ -46,7 +77,7 @@ void Blue_Bullet::Update() {
             is_dead = 1;
             return;
         }else{
-            MoveTo(GetX(), GetY()+6)
+            MoveTo(GetX(), GetY()+6);
         }
     }
 }
