61989db5a9c58bdf1b7df493c469faa6b296de2b
petmemewmew <90765384+petmemewmew@users.noreply.github.com>
2022-06-01 08:29:16 +0800

    Update GameObjects.h

--- GameObjects.h
+++ GameObjects.h
@@ -2,7 +2,39 @@
 #define GAMEOBJECTS_H__
 
 #include "ObjectBase.h"
+#include "GameWorld.h"
 
-class GameObject : public ObjectBase {};
+class GameObject : public ObjectBase {
+protected:
+    GameObject(int imageID, int x, int y, int direction, int layer, double size );
+    int is_dead = 0;
+
+public:
+    int check_dead();
+};
+
+class Dawnbreaker: public GameObject{
+public:
+    Dawnbreaker();
+    void Update() override;
+private:
+    int energy = 10;
+    int HP = 100;
+};
+
+class Star: public GameObject{
+public:
+    Star(int x, int y, double size);
+    void Update() override;
+};
+
+class Blue_Bullet: public GameObject{
+public:
+    Blue_Bullet(int x, int y, double size, int hit);
+    void Update() override;
+
+private:
+    int hurt;
+};
 
 #endif // GAMEOBJECTS_H__
