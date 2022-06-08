483e34de69a485f1723f0a91e42f276521cea959
petmemewmew <90765384+petmemewmew@users.noreply.github.com>
2022-06-08 18:54:30 +0800

    Update Blue_Bullet.cpp

--- PartForYou/Blue_Bullet.cpp
+++ PartForYou/Blue_Bullet.cpp
@@ -1,4 +1,38 @@
 //
 // Created by 26963 on 2022/6/3.
 //
+#include "Blue_Bullet.h"
 
+Blue_Bullet::Blue_Bullet(int x, int y, double size, int hit, GameWorld *world) : GameObject(IMGID_BLUE_BULLET, x, y, 0,
+                                                                                            1, size, world) {
+    hurt = hit;
+}
+
+void Blue_Bullet::Update() {
+    if (is_dead) {
+        return;
+    } else {
+        if (GetY() > WINDOW_HEIGHT) {
+            is_dead = 1;
+            return;
+        } else {
+            MoveTo(GetX(), GetY() + 6);
+        }
+    }
+}
+
+int Blue_Bullet::is_enemy() {
+    return 0;
+}
+
+int Blue_Bullet::is_blue_bullet() {
+    return 1;
+}
+
+int Blue_Bullet::is_meteor() {
+    return 0;
+}
+
+int Blue_Bullet::return_hurt() {
+    return hurt;
+}
