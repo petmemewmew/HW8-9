e9f6a21cce8a8f4766f4c433f2b73d203283cd13
petmemewmew <90765384+petmemewmew@users.noreply.github.com>
2022-06-08 18:54:03 +0800

    Update Blue_Bullet.h

--- PartForYou/Blue_Bullet.h
+++ PartForYou/Blue_Bullet.h
@@ -5,4 +5,29 @@
 #ifndef DAWNBREAKER_BLUE_BULLET_H
 #define DAWNBREAKER_BLUE_BULLET_H
 
+#include "GameObjects.h "
+#include "GameWorld.h"
+
+class GameObject;
+
+class GameWorld;
+
+class Blue_Bullet : public GameObject {
+public:
+    Blue_Bullet(int x, int y, double size, int hit, GameWorld *world);
+
+    void Update() override;
+
+    int is_enemy() override;
+
+    int is_blue_bullet() override;
+
+    int is_meteor() override;
+
+    int return_hurt() override;
+
+private:
+    int hurt;
+};
+
 #endif //DAWNBREAKER_BLUE_BULLET_H
